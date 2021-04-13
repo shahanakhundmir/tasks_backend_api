ca91193022d8614da10c08d2c6d65187388b0545
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-04-13 11:00:17 +0100

    added response body and returning task array as json

--- src/main/java/com/techreturners/Handler.java
+++ src/main/java/com/techreturners/Handler.java
@@ -11,14 +11,23 @@ import org.apache.logging.log4j.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+
+
+
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	private static final Logger LOG = LogManager.getLogger(Handler.class);
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context){
-		LOG.info("received: {}", input);
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context){
+		LOG.info("received the request");
 
 		Task t1 = new Task("12345", "this is task 1",false );
 		Task t2 = new Task("67890", "this is task 2",false );
@@ -28,9 +37,19 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 		tasks.add(t1);	
 		tasks.add(t2);	
 
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(tasks)
-				.build();
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		response.setStatusCode(200);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try{
+			String responseBody = objectMapper.writeValueAsString(tasks);
+			response.setBody(responseBody);
+		}
+		catch(JsonProcessingException e)
+		{
+			LOG.error("unable to marshall tasks array", e);
+		}
+		
+
+		return response;
 	}
 }
