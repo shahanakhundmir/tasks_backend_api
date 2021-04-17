8914cf78af73275361acd2e656b58e4daa4658cd
yogitakumar <helloyogita@gmail.com>
2021-04-17 19:26:28 +0100

    config file

--- src/main/java/com/impact/GetRestaurantHandler.java
+++ src/main/java/com/impact/GetRestaurantHandler.java
@@ -1,4 +1,4 @@
-package com.serverless;
+package com.impact;
 
 import java.util.Collections;
 import com.impact.model.Restaurant;
@@ -34,24 +34,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class GetRestaurantHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	private static final Logger LOG = LogManager.getLogger(GetRestaurantHandler.class);
-	
+
 	private Connection connection= null;
 	private PreparedStatement preparedStatement=null;
 	private ResultSet resultSet = null;
 
 	private void closeConnection(){
 		try{
-		   if(resultSet != null)
-			  resultSet.close();
-		   if(preparedStatement != null)
-			  preparedStatement.close();
-		   if(connection != null)
-			  connection.close();
+			if(resultSet != null)
+				resultSet.close();
+			if(preparedStatement != null)
+				preparedStatement.close();
+			if(connection != null)
+				connection.close();
 		}
 		catch (Exception e){
-		   LOG.error("Unable to close connection to MYSQL - {}",e.getMessage());
+			LOG.error("Unable to close connection to MYSQL - {}",e.getMessage());
 		}
-	 }
+	}
 
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context){
@@ -62,34 +62,38 @@ public class GetRestaurantHandler implements RequestHandler<APIGatewayProxyReque
 
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-   			connection = DriverManager.getConnection(
-         		String.format("jdbc:mysql//%s/%s?user=%s&password=%s","3306",
-				 "nandosinstance.cyvsgwhk3npl.eu-west-2.rds.amazonaws.com", "nandosdb", "root", "xxx"));
-				 preparedStatement = connection.prepareStatement("select * from restaurant");
-				 resultSet = preparedStatement.executeQuery();
+			connection = DriverManager.getConnection(
+					String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
+							System.getenv("DB_HOST"),
+							System.getenv("DB_NAME"),
+							System.getenv("DB_USER"),
+							System.getenv("DB_PASSWORD")));
 
-				 while(resultSet.next()){
-					Restaurant restaurant = new Restaurant(resultSet.getInt("rest_id"),
-									   resultSet.getString("rest_name"),
-									   resultSet.getString("rest_branch"),
-									   resultSet.getString("allergen_safety"),
-									   resultSet.getDate("sys_creation_date"),
-									   resultSet.getDate("sys_update_date"),
-									   resultSet.getInt("user_id"),
-									   resultSet.getString("application_id"),
-									   resultSet.getString("version_code"));
+			preparedStatement = connection.prepareStatement("select * from Restaurant");
+			resultSet = preparedStatement.executeQuery();
 
-					restaurants.add(restaurant);
-				 }
+			while(resultSet.next()){
+				Restaurant restaurant = new Restaurant(resultSet.getInt("rest_id"),
+						resultSet.getString("rest_name"),
+						resultSet.getString("rest_branch"),
+						resultSet.getString("allergen_safety"),
+						resultSet.getDate("sys_creation_date"),
+						resultSet.getDate("sys_update_date"),
+						resultSet.getInt("user_id"),
+						resultSet.getString("application_id"),
+						resultSet.getString("version_code"));
+
+				restaurants.add(restaurant);
+			}
 		}
 		catch (Exception e){
 			LOG.error(String.format("Unable to query database for restaurant"),e);
-	  	}
-	  	finally{
-		closeConnection();
-	 	}
-		
-		
+		}
+		finally{
+			closeConnection();
+		}
+
+
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
 		response.setStatusCode(200);
 		ObjectMapper objectMapper = new ObjectMapper();
@@ -101,26 +105,25 @@ public class GetRestaurantHandler implements RequestHandler<APIGatewayProxyReque
 		{
 			LOG.error("unable to marshall tasks array", e);
 		}
-		
+
 		return response;
 	}
 }
 
 
 /**try{
-			SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
-			String dateString = f.format( new Date());
-			Date date = f.parse ( "2021-01-31" );   
+ SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+ String dateString = f.format( new Date());
+ Date date = f.parse ( "2021-01-31" );
+ //if ( Integer.parseInt(restId) == 001){
+ Restaurant r1 = new Restaurant(001, "this is rest 1","branchx", "blah", date, date, 002, "hhh", "ggg" );
+ restaurants.add(r1);
+ Restaurant r2 = new Restaurant(002, "this is rest 2","branchy", "blahx",date, date, 002, "hhhx", "gggx" );
+ restaurants.add(r2);
+ //}
+ //else{
 
-			//if ( Integer.parseInt(restId) == 001){
-			Restaurant r1 = new Restaurant(001, "this is rest 1","branchx", "blah", date, date, 002, "hhh", "ggg" );
-			restaurants.add(r1);	
-			Restaurant r2 = new Restaurant(002, "this is rest 2","branchy", "blahx",date, date, 002, "hhhx", "gggx" );
-			restaurants.add(r2);	
-			//}
-			//else{
-				
-//}
-		}
-		catch(ParseException e)
-		{LOG.error(e);}*/
+ //}
+ }
+ catch(ParseException e)
+ {LOG.error(e);}*/
